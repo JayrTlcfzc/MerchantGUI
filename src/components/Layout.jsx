a55a9516e18c1961386190c1085ce7a5a1914691
JayrTlcfzc <160154791+JayrTlcfzc@users.noreply.github.com>
2024-12-03 16:53:08 +0800

    added pages

---
+++ src/components/Layout.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Sidebar from "./Navbar/Sidebar";
+
+const Layout = ({ children, username }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  return (
+    <div className="flex h-screen">
+      <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
+      <div className="flex flex-col w-full">
+        <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
+          <div className="text-white text-lg font-medium">{username}</div>
+        </header>
+        <div className="flex-1 bg-gray-100 p-4 overflow-auto">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
