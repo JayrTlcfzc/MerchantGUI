bad9d87238288edd84b456092636863b76b85c5e
AlyannaNcls <alyanna.nicolas@tlcfzc.ae>
2024-12-05 20:12:12 +0800

    fix login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,9 @@ module.exports = {
       fontFamily: {
         quicksand: ['Quicksand', 'sans-serif'], // Add the Quicksand font family here
       },
+      textShadow: { sm: '0 1px 2px rgba(0, 0, 0, 0.5)', default: '0 2px 4px rgba(0, 0, 0, 0.5)', lg: '0 3px 6px rgba(0, 0, 0, 0.5)',
+      },
     },
   },
-  plugins: [],
+  plugins: [ function ({ addBase, config }) { addBase({ '.text-shadow-sm': { textShadow: config('theme.textShadow.sm') }, '.text-shadow': { textShadow: config('theme.textShadow.default') }, '.text-shadow-lg': { textShadow: config('theme.textShadow.lg') }, }); },],
 };
