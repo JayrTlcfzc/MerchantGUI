11b5d298ebc6f7f98a23d21ca18bb7be881ad06d
itsValTlcfzc <allysah.delacruz@tlcfzc.ae>
2024-12-06 19:29:46 +0400

    View pending and web users enhancement.

--- src/pages/AccountManagement/viewPendingSubscriber.jsx
+++ src/pages/AccountManagement/viewPendingSubscriber.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Eye, Search } from "lucide-react";
 
 const ViewPendingSubscriber = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchInput, setSearchInput] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(5);
   const [sortConfig, setSortConfig] = useState({ key: "firstname", direction: "ascending" });
@@ -15,7 +15,7 @@ const ViewPendingSubscriber = () => {
   ];
 
   const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
+    setSearchInput(event.target.value);
   };
 
   const sortedData = [...data].sort((a, b) => {
@@ -30,7 +30,7 @@ const ViewPendingSubscriber = () => {
 
   const filteredData = sortedData.filter((item) =>
     Object.values(item).some((val) =>
-      String(val).toLowerCase().includes(searchTerm.toLowerCase())
+      String(val).toLowerCase().includes(searchInput.toLowerCase())
     )
   );
 
@@ -50,6 +50,7 @@ const ViewPendingSubscriber = () => {
 
   const totalPages = Math.ceil(filteredData.length / itemsPerPage);
 
+  // Pagination Number Rendering Function
   const renderPageNumbers = () => {
     const pageNumbers = [];
     for (let i = 1; i <= totalPages; i++) {
@@ -69,14 +70,34 @@ const ViewPendingSubscriber = () => {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <div className="max-w-4xl mx-auto bg-white p-6 rounded shadow-md">
-        <span className="text-2xl font-semibold text-gray-800 flex items-center justify-center mb-10">
+
+        {/* Page Title */}
+        <h2 className="text-2xl font-semibold text-gray-800 flex items-center justify-center mb-10">
           <Eye color="#D95F08" className="mr-2" /> View Pending Subscriber
-        </span>
+        </h2>
 
         {/* Search Area */}
-        <div className="relative mb-4">
-          <Search className="absolute left-3 top-2.5 text-gray-400" />
-          <input type="text" placeholder="Search..." value={searchTerm} onChange={handleSearch} className="w-1/3 px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent pl-10" /> 
+        <div className="relative flex justify-end gap-4 mb-4">
+          <input
+            type='text'
+            value={searchInput}
+            onChange={handleSearch}
+            placeholder='Search...'
+            className='w-1/5 h-10 border border-gray-400 rounded p-2 focus:outline-none focus:ring-1 focus:ring-[#23587C]' 
+          />
+
+          {!searchInput ? (
+            <Search 
+                color='#BFBFBF'
+                className='absolute right-2 top-1/2 -translate-y-1/2' 
+            />) 
+            : (
+            <X 
+                color='#BFBFBF'
+                onClick={() => setSearchInput('')}
+                className='absolute right-2 top-1/2 -translate-y-1/2' 
+            />) 
+          }
         </div>
 
         {/* Table Content */}
@@ -97,7 +118,7 @@ const ViewPendingSubscriber = () => {
               </th>
             </tr>
           </thead>
-          <tbody className="text-center divide-gray-200">
+          <tbody className="text-center divide-y divide-gray-200">
             {currentItems.length > 0 ? (
               currentItems.map((item, index) => (
                 <tr key={index} className="cursor-pointer">
@@ -121,14 +142,14 @@ const ViewPendingSubscriber = () => {
         <div className="flex justify-center mt-4 space-x-1">
           <button
             onClick={() => paginate(1)}
-            className="px-3 py-1 bg-none text-gray-700 text-xl text-[#19405A] font-bold"
+            className="px-3 py-1 bg-none text-xl text-[#19405A] font-bold"
             disabled={currentPage === 1}
           >
             «
           </button>
           <button
             onClick={() => paginate(currentPage - 1)}
-            className="px-3 py-1 bg-none text-gray-700 text-xl text-[#19405A] font-bold"
+            className="px-3 py-1 bg-none text-xl text-[#19405A] font-bold"
             disabled={currentPage === 1}
           >
             ‹
@@ -136,14 +157,14 @@ const ViewPendingSubscriber = () => {
           {renderPageNumbers()} {/* PAGE NUMBERS */}
           <button
             onClick={() => paginate(currentPage + 1)}
-            className="px-3 py-1 bg-none text-gray-700 text-xl text-[#19405A] font-bold"
+            className="px-3 py-1 bg-none text-xl text-[#19405A] font-bold"
             disabled={currentPage === totalPages}
           >
             ›
           </button>
           <button
             onClick={() => paginate(totalPages)}
-            className="px-3 py-1 bg-none text-gray-700 text-xl text-[#19405A] font-bold"
+            className="px-3 py-1 bg-none text-xl text-[#19405A] font-bold"
             disabled={currentPage === totalPages}
           >
             »
