82a18b7f67f24ba4fa748d39d0fc8e0a43acf5ff
AlyannaNcls <alyanna.nicolas@tlcfzc.ae>
2025-01-08 16:28:05 +0800

    Update Roles Configuration

--- src/api/rolesConfiguration.js
+++ src/api/rolesConfiguration.js
@@ -10,7 +10,7 @@ export const getRolesConfigTable = async (data) => {
     const response = await axios.post(`${BASE_URL}/webuser/rolesConfiguration`, { userLevel: data });
 
     const responseData = response.data;
-    console.log("RESPONSE DATA: " + responseData.roles);
+    console.log("RESPONSE DATA (Roles): " + responseData.roles);
 
     if (responseData.message === "Success") {
       return { success: true, roles: responseData.roles };
@@ -26,5 +26,32 @@ export const getRolesConfigTable = async (data) => {
     return { success: false, message: error.response?.data?.StatusMessage || error.message };
   }
 
+}
+
+export const updateRoles = async (userlevel, id, module, actionStatus) => {
+
+  try {
+    const response = await axios.post(`${BASE_URL}/webuser/updateRoles`, {
+      userlevel,
+      id,
+      module,
+      actionStatus
+    });
+
+    const responseData = response.data;
+
+    if (responseData.message === "Role successfully updated") {
+      return { success: true, newRole: responseData.newRole };
+    } else {
+      console.log(responseData?.StatusMessage || "No message");
+      return { success: false, message: responseData?.StatusMessage || "Unknown error" };
+    }
+  } catch (error) {
+    console.error("Error in React:", error);
+    if (error.response) {
+      console.error("Error Response Data:", error.response.data);
+    }
+    return { success: false, message: error.response?.data?.StatusMessage || error.message };
+  }
 
 }
