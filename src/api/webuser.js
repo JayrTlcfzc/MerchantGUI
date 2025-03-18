724cc6a7df0c620a1b7ea45303e6a2f1f0e3ef4a
AlyannaNcls <alyanna.nicolas@tlcfzc.ae>
2025-03-18 15:19:03 +0800

    Update webuser.js

--- src/api/webuser.js
+++ src/api/webuser.js
@@ -73,7 +73,7 @@ export const userLevelCol = async () => {
       }
     );
 
-      if (response.StatusCode === 97 || response.StatusCode === 93 || response.StatusCode === 98) {
+      if (response.data.StatusCode === 97 || response.data.StatusCode === 93 || response.data.StatusCode === 98) {
         return { logout: true, message: response?.StatusMessage };
       } else {
         return response.data;
